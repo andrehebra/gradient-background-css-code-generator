95f5a010d2ca42dd21ee06aa2be936c1d5dbe6e8
Andre Hebra <andrehebra@gmail.com>
2019-07-22 01:22:09 -0400

    added card centered in the middle and inverted gradient to seperate the labels. also added the css code to copy and paste for other people to maybe use.

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var card = document.getElementById("card");
+var cssLabel = document.getElementById("csscode");
 
 // body.style.background = "red";
 
+cssLabel.innerHTML = "CSS: linear-gradient(to right," + color1.value + "," + color2.value + ")";
+
 function changeBackground(color1, color2) {
   body.style.background = "linear-gradient(to right, " + color1.value + " , " + color2.value + ")";
+  card.style.background = "linear-gradient(to left, " + color1.value + " , " + color2.value + ")";
+  cssLabel.innerHTML = "CSS: linear-gradient(to right," + color1.value + "," + color2.value + ");";
 }
 
 color1.addEventListener("input", function() {
